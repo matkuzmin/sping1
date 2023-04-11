67495dc59bf2498d799eca2c246a94cefc4c5490
matvey <qjlf@inbox.ru>
2023-04-11 13:39:15 +0300

    прошу проверить дз

--- src/main/java/pro/sky/springdemo/СalculatorController.java
+++ src/main/java/pro/sky/springdemo/СalculatorController.java
@@ -1,3 +1,33 @@
 package pro.sky.springdemo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/calculator")
+public class СalculatorController {
+    private final СalculatorService сalculatorService;
 
+    public СalculatorController(СalculatorService сalculatorService) {
+       this.сalculatorService=сalculatorService;
+    }
+
+    @GetMapping
+    public String calculator(){
+        return сalculatorService.calculator();
+    }@GetMapping("/plus")
+    public String summ(@RequestParam int num1,@RequestParam int num2){
+        return сalculatorService.summ(num1,num2);
+    }@GetMapping("/minus")
+    public String minus(@RequestParam int num1,@RequestParam int num2){
+        return сalculatorService.minus(num1,num2);
+    }@GetMapping("/multiply")
+    public String multiply(@RequestParam int num1,@RequestParam int num2){
+        return сalculatorService.multiply(num1,num2);
+    }@GetMapping("/divide")
+    public String divide(@RequestParam int num1, @RequestParam int num2){
+        return сalculatorService.divide(num1,num2);
+    }
+
+}
---
+++ src/main/java/pro/sky/springdemo/СalculatorService.java
@@ -0,0 +1,48 @@
+package pro.sky.springdemo;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+public class СalculatorService {
+    public String calculator() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public String summ(int num1, int num2) {
+        if (String.valueOf(num1) == null || String.valueOf(num1) == null ) {
+            return "Вы не ввели значение переменной";
+        }
+        int sum = num1 + num2;
+        return String.valueOf(sum);
+    }
+
+    public String minus(int num1, int num2) {
+        if (String.valueOf(num1) == null || String.valueOf(num1) == null ) {
+            return "Вы не ввели значение переменной";
+        }
+        int min = num1 - num2;
+        return String.valueOf(min);
+    }
+
+    public String multiply(int num1, int num2) {
+        if (String.valueOf(num1) == null || String.valueOf(num1) == null ) {
+            return "Вы не ввели значение переменной";
+        }
+        int multip = num1 * num2;
+        return String.valueOf(multip);
+    }
+
+    public String divide(int num1, int num2) {
+        if (String.valueOf(num1) == null || String.valueOf(num1) == null ) {
+return "Вы не ввели значение переменной";
+        }
+                if (num2 == 0) {
+                    return "На 0 делить нельзя";
+                }
+
+                int divi = num1 / num2;
+                return String.valueOf(divi);
+            }
+        }
+
